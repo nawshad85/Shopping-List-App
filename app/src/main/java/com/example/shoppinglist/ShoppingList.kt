e2d326a0d1c0e5a704edc2d63aa76061e29ef4ee
nawshad85 <abrarsami2021@gmail.com>
2025-07-30 04:03:06 +0600

    add the working add button

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,9 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,9 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-data class ShoppingList(
+data class ShoppingItem(
     val id: Int,
     val name: String,
     var quantity: Int,
@@ -31,7 +34,7 @@ data class ShoppingList(
 
 @Composable
 fun ShoppingListApp() {
-    var sItems by remember { mutableStateOf(listOf<ShoppingList>()) }
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
     var itemName by remember { mutableStateOf("") }
     var itemQuantity by remember { mutableStateOf("") }
@@ -52,7 +55,7 @@ fun ShoppingListApp() {
                 .padding(16.dp),
         ) {
             items(sItems) {
-
+                ShoppingListItem(it, {}, {})
             }
         }
     }
@@ -66,7 +69,17 @@ fun ShoppingListApp() {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ){
                     Button(onClick = {
-
+                        if(itemName.isNotBlank()){
+                            val newItem = ShoppingItem(
+                                id = sItems.size + 1,
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems = sItems + newItem
+                            showDialog = false
+                            itemName = ""
+                            itemQuantity = ""
+                        }
                     }){
                         Text("Add")
                     }
@@ -94,7 +107,16 @@ fun ShoppingListApp() {
             }
         )
     }
-
 }
 
-
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit){
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(8.dp).border(2.dp, Color.Gray, shape = RoundedCornerShape(8.dp))
+    ) {
+        Text("${item.name} (${item.quantity})", modifier = Modifier.padding(8.dp))
+    }
+}
